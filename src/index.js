06cd924a78b2b22158b0a1f1eded2f0d4b026671
Vaalentin <vaalentin@protonmail.com>
2016-02-02 10:17:00 +0100

    update tests and add functions

---
+++ src/index.js
@@ -0,0 +1,44 @@
+/**
+ * @function create
+ * @param {...uint} fields
+ * @returns {uint}
+ */
+export function create(...fields) {
+
+}
+
+/**
+ * @function combine
+ * @param {...uint} fields
+ * @returns {uint}
+ */
+export function combine(...fields) {
+
+}
+
+/**
+ * @function add
+ * @param {...uint} fields
+ * @returns {uint}
+ */
+export function add(...fields) {
+
+}
+
+/**
+ * @function remove
+ * @param {...uint} fields
+ * @returns {uint}
+ */
+export function remove(...fields) {
+
+}
+
+/**
+ * @function contains
+ * @param {...uint} fields
+ * @returns {uint}
+ */
+export function contains(...fields) {
+
+}
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 import test from 'tape';
+import * as bitmask from '../src/';
 
 const Directions = {
   TOP: 1 << 0,
@@ -7,12 +8,72 @@ const Directions = {
   LEFT: 1 << 3
 }
 
-test('should be able to add and remove fields', t => {
+test('should be able to create a bitmask', t => {
+  t.plan(3);
+
+  let mask;
+
+  mask = bitmask.create();
+
+  t.equal(mask, 0, 'mask is 0 without parameters');
+
+  mask = bitmask.create(Directions.TOP);
+
+  t.equal(mask, Directions.TOP, 'a field can be passed');
+
+  mask = bitmask.create(Directions.TOP, Directions.RIGHT);
+
+  t.equal(mask, Directions.TOP | Directions.RIGHT, 'if several fields are passed they are added')
+});
+
+test('should be able to add fields', t => {
+  t.plan(2);
+
+  let mask = bitmask.create();
+
+  mask = bitmask.add(mask, Directions.TOP);
+  mask = bitmask.add(mask, Directions.RIGHT);
+
+  t.equal(mask, Directions.TOP | Directions.RIGHT, 'can pass a single field');
+
+  mask = bitmask.add(mask, Directions.BOTTOM, Directions.LEFT);
+
+  t.equal(mask, Directions.TOP | Directions.RIGHT | Directions.BOTTOM | Directions.LEFT, 'can pass several fields');
+});
+
+test('should be able to remove fields', t => {
+  t.plan(2);
+
+  let mask = bitmask.create(Directions.TOP | Directions.RIGHT | Directions.LEFT);
+
+  mask = bitmask.remove(mask, Directions.LEFT, 'can pass a single field');
+
+  t.equal(mask, Directions.TOP | Directions.RIGHT);
+
+  mask = bitmask.remove(mask, Directions.TOP, Directions.RIGHT);
+
+  t.equal(mask, 0, 'can pass several fields');
 });
 
 test('should be able to combine fields', t => {
+  t.plan(1);
+
+  let TOP_RIGHT = bitmask.combine(Directions.TOP, Directions.RIGHT);
+
+  t.equal(TOP_RIGHT, Directions.TOP | Directions.RIGHT);
 })
 
-test('should be able to check if a mask contains a field', t => {
+test('should be able to check if a mask contains field', t => {
+  t.plan(4);
+  
+  let mask = bitmask.create(Directions.TOP, Directions.RIGHT, Directions.LEFT);
+
+  const TOP_LEFT = bitmask.combine(Directions.TOP, Directions.LEFT);
+  const BOTTOM_LEFT = bitmask.combine(Directions.BOTTOM, Directions.LEFT);
+
+  t.ok(bitmask.contains(mask, Directions.TOP), 'check for single field');
+  t.ok(bitmask.contains(mask, TOP_LEFT), 'check for combination');
+  t.notOk(bitmask.contains(mask, Directions.BOTTOM), 'check for single field');
+  t.notOk(bitmask.contains(mask, BOTTOM_LEFT), 'check for combination');
 });
 
