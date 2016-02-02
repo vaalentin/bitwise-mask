e56b01ce99e73f79fc5ddb0223acb91c9b6be16b
Vaalentin <vaalentin@protonmail.com>
2016-02-02 12:16:13 +0100

    implement functions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * @returns {uint}
  */
 export function create(...fields) {
-
+  return combine(...fields);
 }
 
 /**
@@ -13,32 +13,55 @@ export function create(...fields) {
  * @returns {uint}
  */
 export function combine(...fields) {
+  let finalField = 0;
 
+  for(let field of fields) {
+    finalField |= field;
+  }
+
+  return finalField;
 }
 
 /**
  * @function add
+ * @param {uint} mask
  * @param {...uint} fields
  * @returns {uint}
  */
-export function add(...fields) {
+export function add(mask, ...fields) {
+  for(let field of fields) {
+    mask |= field;
+  }
 
+  return mask;
 }
 
 /**
  * @function remove
+ * @param {uint} mask
  * @param {...uint} fields
  * @returns {uint}
  */
-export function remove(...fields) {
+export function remove(mask, ...fields) {
+  for(let field of fields) {
+    mask &= ~field;    
+  }
 
+  return mask;
 }
 
 /**
  * @function contains
+ * @param {uint} mask
  * @param {...uint} fields
  * @returns {uint}
  */
-export function contains(...fields) {
+export function contains(mask, ...fields) {
+  for(let field of fields) {
+    if((mask & field) !== field) {
+      return false;
+    }
+  }
 
+  return true;
 }
