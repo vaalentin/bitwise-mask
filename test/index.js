c8e0886845517afd0ae087c6f1e80947811c38b0
Vaalentin <vaalentin@protonmail.com>
2016-02-02 08:28:08 +0100

    add base tests

---
+++ test/index.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+
+const Directions = {
+  TOP: 1 << 0,
+  RIGHT: 1 << 1,
+  BOTTOM: 1 << 2,
+  LEFT: 1 << 3
+}
+
+test('should be able to add and remove fields', t => {
+});
+
+test('should be able to combine fields', t => {
+})
+
+test('should be able to check if a mask contains a field', t => {
+});
+
